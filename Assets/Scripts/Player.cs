6715f5c944e6ec6f9d3c4132d4db012f381ba515
AdamMakoun <adammakaeu@gmail.com>
2023-03-11 21:19:09 +0100

    added colliders and planet class to planet prefabs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
     Planet homePlanet;
     public List<Resource> resources = new List<Resource>();
     public Upgrades playerUpgrades = new Upgrades();
-    List<Spaceship> spaceships = new List<Spaceship>();
+    List<Spaceship> spaceshipsOnPlanet = new List<Spaceship>();
+    List<Spaceship> spaceshipsOffPlanet = new List<Spaceship>();
     public Player(Planet homePlanet)
     {
         this.homePlanet = homePlanet;
@@ -16,9 +17,21 @@ public class Player
     {
         foreach(Resource ore in resources)
         {
-
+            if(ore.name == "" && ore.amm > 1)
+            {
+                spaceshipsOnPlanet.Add(new Spaceship(playerUpgrades,false));
+            }
         }
         
+    }
+    public void AttackPlanet(int ammOfSentUnits)
+    {
+        for (int i = 0; i < ammOfSentUnits; i++)
+        {
+            spaceshipsOffPlanet.Add(spaceshipsOnPlanet[i]);
+            spaceshipsOnPlanet.RemoveAt(i);
+
+        }
     }
     
 }
--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spaceship
+public class Spaceship : MonoBehaviour
 {
     bool isEnemy;
     Sprite sprite;
     float attack;
     float speed;
-    public Spaceship(Upgrades upgrades, Sprite sprite, bool isEnemy)
+    public Spaceship(Upgrades upgrades, bool isEnemy)
     {
         this.isEnemy = isEnemy;
-        this.sprite = sprite;
+        //sprite = 
         attack = 10 * upgrades.attackAndSpeedUpgrades;
         speed = 5 * upgrades.defenceUpgrades;
     }
