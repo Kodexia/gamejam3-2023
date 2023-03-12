8e0634bc5a6f47a973fa0ef3960f69166f7836cc
Ejdyz <adamhonzikcz@seznam.cz>
2023-03-12 01:14:30 +0100

    animation between scenes and about scene created

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 2f;
+
+    public void LoadNextLevel(int id)
+    {
+        StartCoroutine(LoadLevel(id));
+    }
+
+    IEnumerator LoadLevel(int SceneId)
+    {
+        //play animation
+        transition.SetTrigger("Load");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(SceneId);
+    }
+
+}
