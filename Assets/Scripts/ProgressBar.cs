59437f17f57b1e44ea7fe380da9a8754bdf50a10
Ejdyz <adamhonzikcz@seznam.cz>
2023-03-12 03:33:46 +0100

    Added ProgressBar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int maximum;
+    public int progress;
+    public Image mask;
+    public Image fill;
+    public Color color;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)progress / (float)maximum;
+        mask.fillAmount = fillAmount;
+        fill.color = color;
+
+    }
+}
