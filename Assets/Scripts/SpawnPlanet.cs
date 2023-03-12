b9e9ee98e82689a9fee08c750bfd4cfcc997b21a
Fami6Xx <libojer@gmail.com>
2023-03-12 12:10:08 +0100

    fix: some fixes to spawning, still needs fixing tho

--- Assets/Scripts/SpawnPlanet.cs
+++ Assets/Scripts/SpawnPlanet.cs
@@ -12,12 +12,13 @@ public class SpawnPlanet : MonoBehaviour
     
 
     [SerializeField]
-    public List<GameObject> sprite;
+    public List<GameObject> planets;
+    [SerializeField]
+    public List<GameObject> asteroids;
+
 
     [SerializeField]
-    public int planets = 8;
-    [SerializeField]
-    public int randomPlanet = 0;
+    public int spawnedPlanets = 8;
 
     [SerializeField]
     float distanceBetweenPoints = 10f;
@@ -30,11 +31,13 @@ public class SpawnPlanet : MonoBehaviour
     [SerializeField]
     public GameObject homePlanet;
 
-    float minDistance = 4f; // minimum distance between positions
     public List<Vector2> positions;
+    Dictionary<Vector2, GameObject> generatedPositions = new Dictionary<Vector2, GameObject>();
+
+    // Problém je že mìnìní transformu nezmìní actually ten gameobject, a mnì už došly nápady :/
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         // Get the position (X,Y) of the sprite
         SpriteRenderer spriteRenderer = bg.GetComponent<SpriteRenderer>();
@@ -46,39 +49,96 @@ public class SpawnPlanet : MonoBehaviour
 
         List<Vector2> vectors = GeneratePoints(distanceBetweenPoints, spriteRenderer.sprite.bounds.size);
 
-        if (sprite.Count < planets)
+
+        if (planets.Count < spawnedPlanets)
         {
-            planets = sprite.Count;
-        }
-
-        for (int i = 0; i < planets; i++)
-        {
-            randomPlanet = UnityEngine.Random.Range(0, sprite.Count - 1);
-
-            GameObject planet = sprite[randomPlanet];
-
-            Instantiate(planet);
-
-            Vector2 chosenVector = vectors[UnityEngine.Random.Range(0, vectors.Count)];
-
-            Vector2 spawnPosition = new Vector2(posX + chosenVector.x, posY + chosenVector.y);
-
-            planet.transform.position = spawnPosition;
-            positions.Add(spawnPosition);
+            spawnedPlanets = planets.Count;
         }
 
         Instantiate(homePlanet);
-        homePlanet.transform.position = new Vector2(0, 0);
-        positions.Add(new Vector2(0, 0));
+
+        Vector2 homePlanetSpawnVector = new Vector2(posX, posY) + new Vector2(width, height);
+
+        homePlanet.transform.SetPositionAndRotation(homePlanetSpawnVector, Quaternion.identity);
+        homePlanet.name = "Home planet";
+        positions.Add(homePlanetSpawnVector);
+
+        vectors.RemoveAt(0);
+
+        for (int i = 0; i < spawnedPlanets; i++)
+        { 
+            if(vectors.Count == 0)
+            {
+                break;
+            }
+
+            GameObject planet = planets[UnityEngine.Random.Range(0, planets.Count - 1)];
+
+            Instantiate(planet);
+
+            int chosenIndex = UnityEngine.Random.Range(0, vectors.Count);
+            Vector2 chosenVector = vectors[chosenIndex];
+
+            Debug.Log($"Planet {planet.name} spawned at {chosenVector + new Vector2(posX, posY)}");
+            
+
+            Vector2 spawnPosition = chosenVector + new Vector2(posX, posY);
+
+            planet.transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
+            Debug.Log("Transform pos: " + planet.transform.position);
+            Debug.Log("Local transform: " + planet.transform.localPosition);
+            positions.Add(spawnPosition);
+            generatedPositions.Add(spawnPosition, planet);
+
+            vectors.RemoveAt(chosenIndex);
+        }
+
+        while(vectors.Count > 0)
+        {
+            Vector2 spawnPosition = vectors[0] + new Vector2(posX, posY);
+            vectors.RemoveAt(0);
+
+            GameObject asteroid = asteroids[UnityEngine.Random.Range(0, asteroids.Count)];
+            Instantiate(asteroid);
+            asteroid.transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
+
+            positions.Add(spawnPosition);
+        }
     }
 
-    public static List<Vector2> GeneratePoints(float radius, Vector2 sampleRegionSize, int numSamplesBeforeRejection = 30)
+    void LateUpdate()
+    {
+        for(int i = 0; i < generatedPositions.Count; i++)
+        {
+            Vector2 desiredPosition = generatedPositions.Keys.ToArrayPooled()[i];
+
+            GameObject planet;
+            generatedPositions.TryGetValue(desiredPosition, out planet);
+
+            if (planet == null) continue;
+
+            Vector2 currentPosition = planet.transform.position;
+            if(desiredPosition != currentPosition)
+            {
+                Debug.Log("Fixing pos for " + planet.name);
+                Debug.Log("Current pos: " + currentPosition);
+                Debug.Log("Desired pos: " + desiredPosition);
+                Debug.Log("--");
+                planet.transform.position = new Vector2(desiredPosition.x, desiredPosition.y); // Tadyto prostì nezmìní pozici ani za hovno, idk proè
+            }
+        }
+    }
+
+    public List<Vector2> GeneratePoints(float radius, Vector2 sampleRegionSize, int numSamplesBeforeRejection = 30)
     {
         float cellSize = radius / Mathf.Sqrt(2);
 
         int[,] grid = new int[Mathf.CeilToInt(sampleRegionSize.x / cellSize), Mathf.CeilToInt(sampleRegionSize.y / cellSize)];
         List<Vector2> points = new List<Vector2>();
         List<Vector2> spawnPoints = new List<Vector2>();
+
+        points.Add(sampleRegionSize / 2);
+        grid[(int)(points[0].x / cellSize), (int)(points[0].y / cellSize)] = points.Count;
 
         spawnPoints.Add(sampleRegionSize / 2);
         while (spawnPoints.Count > 0)
@@ -106,12 +166,39 @@ public class SpawnPlanet : MonoBehaviour
                 spawnPoints.RemoveAt(spawnIndex);
             }
 
+        } 
+
+        // Final check
+        for(int i = 0; i < points.Count; i++)
+        {
+            for(int j = 0; j < points.Count; j++)
+            {
+                if(i == j) continue;
+                Vector2 father = points[i];
+                Vector2 son = points[j];
+
+                Debug.Log($"{radius} >= {Vector2.Distance(father, son)}");
+
+                if(radius >= Vector2.Distance(father, son))
+                {
+                    if (UnityEngine.Random.Range(0, 2) == 0)
+                    {
+                        Debug.LogError("Found bugged spawn, removing father");
+                        points.RemoveAt(i);
+                    }
+                    else
+                    {
+                        Debug.LogError("Found bugged spawn, removing son");
+                        points.RemoveAt(j);
+                    }
+                }
+            }
         }
 
         return points;
     }
 
-    static bool IsValid(Vector2 candidate, Vector2 sampleRegionSize, float cellSize, float radius, List<Vector2> points, int[,] grid)
+    bool IsValid(Vector2 candidate, Vector2 sampleRegionSize, float cellSize, float radius, List<Vector2> points, int[,] grid)
     {
         if (candidate.x >= 0 && candidate.x < sampleRegionSize.x && candidate.y >= 0 && candidate.y < sampleRegionSize.y)
         {
