9e48bb44c49d30f7e9212a4a492badc7d611c2f1
HuricanSpeed <mcg03241@gmail.com>
2023-03-12 04:26:13 +0100

    Going to some place

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -9,7 +9,7 @@ public class Spaceship : MonoBehaviour
     float attack;
     float speed;
     [SerializeField]
-    public Vector2 whereToGo = new Vector2(10, 10);
+    public Vector2 whereToGo = new Vector2(0, 0);
     public Spaceship(Upgrades upgrades, bool isEnemy)
     {
         this.isEnemy = isEnemy;
@@ -17,25 +17,34 @@ public class Spaceship : MonoBehaviour
         attack = 10 * upgrades.attackAndSpeedUpgrades;
         speed = 5 * upgrades.defenceUpgrades;
     }
-    
+
+    private void Update()
+    {
+        if(whereToGo != new Vector2(0, 0))
+        {
+            /*Vector2 distance = new Vector2(transform.position.x , transform.position.y);*/
+            if (transform.position.y >= whereToGo.y - 0.1 && transform.position.y <= whereToGo.y + 0.1 && transform.position.x >= whereToGo.x - 0.1 && transform.position.x <= whereToGo.x + 0.1)
+            {
+                transform.position = new Vector2(whereToGo.x, whereToGo.y);
+                whereToGo = new Vector2(0, 0);
+            }
+
+            // Calculate the direction to the target position
+            Vector3 direction = (whereToGo - (Vector2)transform.position).normalized;
+
+            // Move the object towards the target position
+            transform.Translate(direction * Time.deltaTime, Space.World);
+
+            // Calculate the angle to the target position
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Rotate the object towards the target position only on the z-axis
+            transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+        }
+    }
+
     public void moveOnTo(Vector2 toGo)
     {
-        /*Vector2 distance = new Vector2(transform.position.x , transform.position.y);*/
-        if (transform.position.y >= toGo.y - 0.1 && transform.position.y <= toGo.y + 0.1 && transform.position.x >= toGo.x - 0.1 && transform.position.x <= toGo.x + 0.1)
-        {
-            transform.position = new Vector2(toGo.x, toGo.y);
-        }
-
-        // Calculate the direction to the target position
-        Vector3 direction = (toGo - (Vector2)transform.position).normalized;
-
-        // Move the object towards the target position
-        transform.Translate(direction * speed * Time.deltaTime, Space.World);
-
-        // Calculate the angle to the target position
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // Rotate the object towards the target position only on the z-axis
-        transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+        whereToGo = toGo;
     }
 }
